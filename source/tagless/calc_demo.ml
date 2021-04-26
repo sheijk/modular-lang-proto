c07bdd54cd1cf08975172e27198d2e984fa87de0
Jan Rehders <nospam@sheijk.net>
2021-04-26 04:07:51 +0200

    Using first class module for tests

--- source/tagless/calc_demo.ml
+++ source/tagless/calc_demo.ml
@@ -1,24 +1,21 @@
 
-module Tests_int(L : Calc_int.Lang) =
-struct
-  let tests = L.[
-      3, int 1 + int 2;
-      1, int 1;
-      99, int 99;
-      106, int 100 + int 3 * int 2;
-    ]
-end
+let int_tests (type t) (module L : Calc_int.Lang with type t = t) : (int * t) list =
+  L.[
+    3, int 1 + int 2;
+    1, int 1;
+    99, int 99;
+    106, int 100 + int 3 * int 2;
+  ]
 
 let test_int() =
-  let module C = Tests_int(Calc_int.To_string) in
-  let module E = Tests_int(Calc_int.Eval) in
   List.iter2 (fun (expected, string) (_, f) ->
       let result = f() in
       if expected = result then
         Printf.printf "ok  %s => %d\n" string result
       else
         Printf.printf "err %s => %d, expected %d\n" string result expected)
-    C.tests E.tests
+    (int_tests (module Calc_int.To_string))
+    (int_tests (module Calc_int.Eval))
 
 module Tests(L : Calc_bool.Lang) =
 struct
