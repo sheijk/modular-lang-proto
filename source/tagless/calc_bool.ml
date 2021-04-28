edfcd8e2ddcac94ebfc6235b3774f94b0d18d5fc
Jan Rehders <nospam@sheijk.net>
2021-04-28 22:06:33 +0200

    Parametric modules so they can be composed

--- source/tagless/calc_bool.ml
+++ source/tagless/calc_bool.ml
@@ -7,21 +7,30 @@ sig
   val ( || ) : t -> t -> t
 end
 
-module To_string =
+module To_string'(T : sig type t = string end) =
 struct
-  type t = string
-  let bool b = if b then "true" else "false"
+  let bool b : T.t = if b then "true" else "false"
   let ( && ) lhs rhs = Printf.sprintf "(%s and %s)" lhs rhs
   let ( || ) lhs rhs = Printf.sprintf "(%s or %s)" lhs rhs
 end
+
+module To_string = struct
+  type t = string
+  include To_string'(struct type t = string end)
+end
 let () = let module T : Lang = To_string in ()
+
+module Eval'(T : sig type t = unit -> bool end) =
+struct
+  let bool b : T.t = fun () -> b
+  let ( && ) lhs rhs = fun () -> ((lhs()) && (rhs()))
+  let ( || ) lhs rhs = fun () -> ((lhs()) || (rhs()))
+end
 
 module Eval =
 struct
   type t = unit -> bool
-  let bool b = fun () -> b
-  let ( && ) lhs rhs = fun () -> ((lhs()) && (rhs()))
-  let ( || ) lhs rhs = fun () -> ((lhs()) || (rhs()))
+  include Eval'(struct type t = unit -> bool end)
 end
 let () = let module T : Lang = Eval in ()
 
--- source/tagless/calc_int.ml
+++ source/tagless/calc_int.ml
@@ -9,25 +9,34 @@ module type Lang =
   val ( / ) : t -> t -> t
 end
 
-module To_string =
+module To_string'(T : sig type t = string end) =
 struct
-  type t = string
-  let int = string_of_int
+  let int : int -> T.t = string_of_int
   let ( + ) lhs rhs = Printf.sprintf "(%s + %s)" lhs rhs
   let ( - ) lhs rhs = Printf.sprintf "(%s - %s)" lhs rhs
   let ( * ) lhs rhs = Printf.sprintf "(%s * %s)" lhs rhs
   let ( / ) lhs rhs = Printf.sprintf "(%s / %s)" lhs rhs
 end
+
+module To_string = struct
+  type t = string
+  include To_string'(struct type t = string end)
+end
 let () = let module T : Lang = To_string in ()
 
-module Eval =
+module Eval'(T : sig type t = unit -> int end) =
 struct
-  type t = unit -> int
-  let int n = fun () -> n
+  let int n : T.t = fun () -> n
   let ( + ) lhs rhs = fun () -> (lhs()) + (rhs())
   let ( - ) lhs rhs = fun () -> (lhs()) - (rhs())
   let ( * ) lhs rhs = fun () -> (lhs()) * (rhs())
   let ( / ) lhs rhs = fun () -> (lhs()) / (rhs())
+end
+
+module Eval =
+struct
+  type t = unit -> int
+  include Eval'(struct type t = unit -> int end)
 end
 let () = let module T : Lang = Eval in ()
 
