0e0b9d325d816895ef6056b5a9f8734524200a44
Jan Rehders <nospam@sheijk.net>
2021-04-28 23:21:38 +0200

    Don't use first-class modules for tests, anymore
    Parametrized types are not supported for function local type abstractions. We
    need those to make languages composable.
    
    Reverting revision c07bdd5

--- source/tagless/calc_demo.ml
+++ source/tagless/calc_demo.ml
@@ -1,21 +1,24 @@
 
-let int_tests (type t) (module L : Calc_int.Lang with type t = t) : (int * t) list =
-  L.[
-    3, int 1 + int 2;
-    1, int 1;
-    99, int 99;
-    106, int 100 + int 3 * int 2;
-  ]
+module Tests_int(L : Calc_int.Lang) =
+struct
+  let tests = L.[
+      3, int 1 + int 2;
+      1, int 1;
+      99, int 99;
+      106, int 100 + int 3 * int 2;
+    ]
+end
 
 let test_int() =
+  let module C = Tests_int(Calc_int.To_string) in
+  let module E = Tests_int(Calc_int.Eval) in
   List.iter2 (fun (expected, string) (_, f) ->
       let result = f() in
       if expected = result then
         Printf.printf "ok  %s => %d\n" string result
       else
         Printf.printf "err %s => %d, expected %d\n" string result expected)
-    (int_tests (module Calc_int.To_string))
-    (int_tests (module Calc_int.Eval))
+    C.tests E.tests
 
 module Tests(L : Calc_bool.Lang) =
 struct
