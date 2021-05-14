84ac8d9a9245822fabdf215f342d59ab51760681
Jan Rehders <nospam@sheijk.net>
2021-05-15 01:24:06 +0200

    Parse to/from string tree

--- source/tagless/algo/algo_bindings.ml
+++ source/tagless/algo/algo_bindings.ml
@@ -50,3 +50,65 @@ struct
   include Bindings.Optimize(L)
 end
 let () = let module T : Lang = Optimize(Count_ast_size) in ()
+
+module String_tree =
+struct
+  type t =
+    | Leaf of string
+    | Tree of t list
+
+  module Build =
+  struct
+    let s str = Leaf str
+    let t children = Tree children
+
+    let test =
+      t [s "int"; s "7"]
+  end
+end
+
+module Attempt1 =
+struct
+  module type Parsable =
+  sig
+    type 'a t
+    val readers : (string * (String_tree.t list -> bool t option)) list
+  end
+
+  module Parse(P : Parsable) =
+  struct
+    let parse tree =
+      let translate hd args =
+        try
+          let _, parse = List.find (fun (handled, _) -> handled = hd) P.readers in
+          parse args
+        with Failure _ ->
+          failwith "unknown language form"
+      in
+      match tree with
+      | String_tree.Tree (String_tree.Leaf hd :: args) ->
+        translate hd args
+      | _ -> failwith "expected a tree"
+  end
+
+  module Parse_bool(L : Calc_bool.Lang) : Parsable =
+  struct
+    type 'a t = 'a L.t
+
+    let bool = function
+      | [String_tree.Leaf "true"] -> Some (L.bool true)
+      | [String_tree.Leaf "false"] -> Some (L.bool true)
+      | _ -> None
+
+    let readers = [
+      "bool", bool;
+      (* "&&", and_; *)
+      (* "||", or_; *)
+    ]
+  end
+end
+
+
+module Attempt2 =
+struct
+end
